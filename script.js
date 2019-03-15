34574d1d2808c85f6231b9879f9524e85e659283
karimulla.shaik@rapidops.com <karimulla1696@gmail.com>
2019-03-15 19:08:42 +0530

    New Updated

--- script.js
+++ script.js
@@ -1097,3 +1097,157 @@ min(1, 1) == 1
 // let isTeaWanted = confirm ("Do you want some tea?");
 // console.log("visitor: " +userName); //karimulla
 // console.log("Tea Wanted: "+ isTeaWanted); //true
+
+
+
+//objects we can add remove read delete
+// let user = {
+//    name: 'kareem',
+//     age: 30,
+//    isAdmin:true,
+//    "likes birds": true
+// }
+// console.log(user);
+
+
+// Task 1.) Hello, object
+// importance: 5
+
+// Write the code, one line for each action:
+
+//     Create an empty object user.
+//     Add the property name with the value John.
+//     Add the property surname with the value Smith.
+//     Change the value of the name to Pete.
+//     Remove the property name from the object.
+
+// let user = {};
+// user.name= "john";
+// user.surname= "smith";
+// user.name= "pete";
+// delete user.name;
+
+
+/* 
+Check for emptiness
+importance: 5
+
+Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+
+Should work like that:
+
+let schedule = {};
+
+alert( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "get up";
+
+alert( isEmpty(schedule) ); // false
+*/
+
+
+
+// function isEmpty(obj) {
+//    for (let key in obj){
+//       //if the loop has started there is a property
+//       return false;
+//    }
+//    return true;
+// }
+
+
+
+/*
+Constant objects?
+importance: 5
+
+Is it possible to change an object declared with const? What do you think?
+
+
+
+
+
+
+
+const user = {
+ name: "John"
+};
+
+// does it work?
+user.name = "Pete";
+*/
+
+
+
+//sure it works, no problem.
+
+// const user= {
+//    name: 'john'
+// };
+//works
+// user.name= 'kareem';
+//error
+// user= 123;
+
+
+/*
+Sum object properties
+importance: 5
+
+We have an object storing salaries of our team:
+
+let salaries = {
+ John: 100,
+ Ann: 160,
+ Pete: 130
+}
+*/
+
+// let salaries= {
+//    john: 100,
+//    kareem: 162,
+//    vamsi: 500
+// };
+// let sum= 0;
+// for (let key in salaries) {
+//    sum +=salaries[key];
+// }
+// console.log(sum); //762
+
+/*
+Multiply numeric properties by 2
+importance: 3
+
+Create a function multiplyNumeric(obj) that multiplies all numeric properties of obj by 2.
+
+For instance:
+
+// before the call
+let menu = {
+ width: 200,
+ height: 300,
+ title: "My menu"
+};
+
+multiplyNumeric(menu);
+
+// after the call
+menu = {
+ width: 400,
+ height: 600,
+ title: "My menu"
+};
+*/
+
+
+// function multiplyNumeric(obj) {
+//    for (let key in obj){
+//       if (typeof obj[key] == 'number') {
+//          obj[key] *= 2;
+//       }
+//    }
+// }
+
+
+
+
